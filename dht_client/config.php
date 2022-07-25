78b5bf1705ae1f2c243b5ea82441486a2f1ff319
ixiaofeng <ixiaofeng.com@gmail.com>
2022-07-26 01:04:22 +0800

    优化

--- dht_client/config.php
+++ dht_client/config.php
@@ -10,7 +10,7 @@ return array(
     'server_port' => 2345, //服务端端口
     'max_conn' => 65535, //最大连接数
     'reload_async' => true, //设置为 true 时，将启用异步安全重启特性，Worker 进程会等待异步事件完成后再退出
-    'max_request' => 0, //防止 PHP 内存溢出, 一个工作进程处理 X 次任务后自动重启 (注: 0,不自动重启)
+    'max_request' => 500000, //防止 PHP 内存溢出, 一个工作进程处理 X 次任务后自动重启 (注: 0,不自动重启)
     'max_wait_time' => 3, //worker退出之前最大等待时间
     'dispatch_mode' => 1, //收到会轮循分配给每一个 Worker 进程
     'discard_timeout_request' => false, //是否丢弃已关闭链接的数据请求
@@ -19,6 +19,6 @@ return array(
     'heartbeat_check_interval' => 5, //启用心跳检测，此选项表示每隔多久轮循一次，单位为秒
     'heartbeat_idle_time' => 10, //与heartbeat_check_interval配合使用。表示连接最大允许空闲的时间
     'task_enable_coroutine' => true, //task协程开启
-    'task_max_request' => 0, //防止 PHP 内存溢出, 一个task进程处理 X 次任务后自动重启 (注: 0,不自动重启)
+    'task_max_request' => 1000000, //防止 PHP 内存溢出, 一个task进程处理 X 次任务后自动重启 (注: 0,不自动重启)
     'enable_coroutine' => true, //开启协程
 );
